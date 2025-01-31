0b9124f0c15ed8f8de331ed70c0b08ae253bb8d9
Kaishya <desai.kaishya@gmail.com>
2025-01-31 07:00:20 +0000

    Initial Commit

---
+++ Asare/AsareApp.swift
@@ -0,0 +1,17 @@
+//
+//  AsareApp.swift
+//  Asare
+//
+//  Created by Kaishya Desai on 31/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct AsareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
